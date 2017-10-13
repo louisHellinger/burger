3d0990affa456bf5b9a512150a56ba62b109ccd0
louis hellinger <louishellinger@louiss-MacBook-Pro.local>
2017-10-13 10:23:26 -0400

    another push

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
   password: "password",
   database: "burgers_db"
 });
+}
 
 
 connection.connect(function(err) {
@@ -17,6 +21,8 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+
 
 
 module.exports = connection;
